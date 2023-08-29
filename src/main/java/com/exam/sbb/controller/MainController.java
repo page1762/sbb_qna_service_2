ab5146774286273fdad97ca593acb15d598fa7b2
page1762 <page1762@gmail.com>
2023-08-29 20:45:55 +0900

    GetMapping, PostMapping, RequestMapping의 차이 설명

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -1,14 +1,41 @@
 package com.exam.sbb.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
   @RequestMapping("/sbb")
   @ResponseBody
   public String index(){
-    return "안녕하세요.";
+    return "안녕하세요.asdasd";
+  }
+  @GetMapping("/page1")
+  @ResponseBody
+  public String showGet(){
+    return """
+        <form method="POST" action="/page2"/>
+            <input type="number" name="age" placeholder="나이 입력" />
+            <input type="submit" value="page2로 post방식으로 이동" />
+        </form>
+        """;
+  }
+
+  @PostMapping("/page2")
+  @ResponseBody
+  public String showPage2Post(@RequestParam(defaultValue = "0") int age){
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. POST방식으로 오신걸 환영합니다.</h1>
+        """.formatted(age);
+  }
+
+  @GetMapping("/page2")
+  @ResponseBody
+  public String showPost(@RequestParam(defaultValue = "0") int age){
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. GET방식으로 오신걸 환영합니다.</h1>
+        """.formatted(age);
   }
 }
