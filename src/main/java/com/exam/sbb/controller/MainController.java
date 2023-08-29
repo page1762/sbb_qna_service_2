bac49dadaed500fdd0f2663fef59a86157479b4c
page1762 <page1762@gmail.com>
2023-08-29 20:53:47 +0900

    plus, minus, increase

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
+
+  private int increaseNo = -1;
   @RequestMapping("/sbb")
   @ResponseBody
   public String index(){
@@ -37,5 +39,24 @@ public class MainController {
         <h1>입력된 나이 : %d</h1>
         <h1>안녕하세요. GET방식으로 오신걸 환영합니다.</h1>
         """.formatted(age);
+  }
+
+  @GetMapping("/plus")
+  @ResponseBody
+  public int showPlus(int a, int b){
+    return a + b;
+  }
+
+  @GetMapping("/minus")
+  @ResponseBody
+  public int showMinus(int a, int b){
+    return a - b;
+  }
+
+  @GetMapping("/increase")
+  @ResponseBody
+  public int showIncrease(){
+    increaseNo++;
+    return increaseNo;
   }
 }
